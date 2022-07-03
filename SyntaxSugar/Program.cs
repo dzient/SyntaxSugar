597875b7647b0e9501d18c3e3b2f35dc00508a82
dzient <34776686+dzient@users.noreply.github.com>
2022-07-03 10:56:32 -0400

    Add files via upload
    First commit

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace SyntaxSugar
+{
+    class Program
+    {   
+        // Given an if-else statement, replace it as a ternary operator
+        // Print out the results for good measure
+        // The real if-else statement is provided as comments
+        public static void Main(String[] args)
+        {
+            int answer = 4;
+            string response;
+
+            response = (answer < 9) ? answer + " is less than nine" : answer + " greater than or equal to nine";
+
+            Console.WriteLine(response);
+            /*
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + “greater than or equal to nine”;
+            }
+            */
+        }
+    }
+}
